5585dcdc69a657ea42d7080cce99f44eeec4437b
Ostap Maliuvanchuk <ostap_mal@hotmail.com>
2014-10-05 09:30:46 +0300

    add heroku anti-sleep

--- src/main/java/com/maliuvanchuk/Application.java
+++ src/main/java/com/maliuvanchuk/Application.java
@@ -4,13 +4,33 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Scheduled(initialDelay=1000, fixedRate=1000 * 60 * 10)
+    public void doSomething() throws IOException {
+        //TODO get IP automatically
+        String url = "http://stormtroopers421.heroku.com";
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("Response code = " + responseCode);
     }
 }
